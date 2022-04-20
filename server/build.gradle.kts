8def8c2caaf62469e871aacd06feccfd250ce865
teodoro98 <teodosiobla@gmail.com>
2022-04-20 18:30:06 +0200

    Andrea

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.postgresql:postgresql")
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
