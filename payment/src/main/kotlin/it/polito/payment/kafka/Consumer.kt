2758c2780b2dd33aaf366c8f05d76b2e652f5380
Andrea Colli-Vignarelli <s290348@studenti.polito.it>
2022-06-01 13:26:14 +0200

    Changes by pushz

--- payment/src/main/kotlin/it/polito/payment/kafka/Consumer.kt
+++ payment/src/main/kotlin/it/polito/payment/kafka/Consumer.kt
@@ -1,4 +1,18 @@
 package it.polito.payment.kafka
 
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.Acknowledgment
+import org.springframework.stereotype.Component
+
+@Component
 class Consumer {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @KafkaListener(topics = ["\${kafka.topics.product}"], groupId = "ppr")
+    fun listenGroupFoo(consumerRecord: ConsumerRecord<Any, Any>, ack: Acknowledgment) {
+        logger.info("Message received {}", consumerRecord)
+        ack.acknowledge()
+    }
 }
--- payment/src/main/kotlin/it/polito/payment/kafka/ProductDeserialize.kt
+++ payment/src/main/kotlin/it/polito/payment/kafka/ProductDeserialize.kt
@@ -1,4 +1,26 @@
 package it.polito.payment.kafka
 
-class ProductDeserialize {
+import com.fasterxml.jackson.databind.ObjectMapper
+import it.polito.payment.dto.OrderTopic
+import org.apache.kafka.common.errors.SerializationException
+import org.apache.kafka.common.serialization.Deserializer
+import org.slf4j.LoggerFactory
+import kotlin.text.Charsets.UTF_8
+
+
+class ProductDeserializer : Deserializer<OrderTopic> {
+    private val objectMapper = ObjectMapper()
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    override fun deserialize(topic: String?, data: ByteArray?): OrderTopic? {
+        log.info("Deserializing...")
+        return objectMapper.readValue(
+            String(
+                data ?: throw SerializationException("Error when deserializing byte[] to Order"), UTF_8
+            ), OrderTopic::class.java
+        )
+    }
+
+    override fun close() {}
+
 }
